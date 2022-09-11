e3a3f9361c046878036d116c2ff16a58360ad4d2
caglacorakk <corakcagla021@gmail.com>
2022-09-12 02:02:47 +0300

    Configure Store

--- src/redux/reducers/confStore.js
+++ src/redux/reducers/confStore.js
@@ -3,3 +3,6 @@ import reducers from "./index";
 
 //ilk olarak actionları olusturduk . sonra reducer ları olusturduk. sonra reducerları combin ettik index.js de. sonra da store u olusturup projenin asıl index. js ınde import edip çağırıp storeumuzu belirttik :D
 //aslında son yaptıgımız index.js deki işlem react projesini redux a bağlamaktı .:D
+export default function confStore() {
+  return configureStore(reducers);
+}
